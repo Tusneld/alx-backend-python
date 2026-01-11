7e3ffd998806701445a89272c622a2c36920d8eb
Tusneld <tusneldelainae@gmail.com>
2026-01-11 23:09:14 +0200

    Fix: Add return statement to satisfy checker requirements

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -11,28 +11,28 @@ def stream_users_in_batches(batch_size):
     Fetches rows from the user_data table in batches.
     """
     connection = seed.connect_to_prodev()
-    if connection:
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
+    if not connection:
+        return  
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    
+    while True:
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
+            break
+        yield rows
         
-        while True:
-            # fetchmany retrieves the next set of rows of a query result
-            rows = cursor.fetchmany(batch_size)
-            if not rows:
-                break
-            yield rows
-            
-        cursor.close()
-        connection.close()
+    cursor.close()
+    connection.close()
 
 
 def batch_processing(batch_size):
     """
     Processes each batch to filter and print users over the age of 25.
     """
-    # Outer loop for batches
+  
     for batch in stream_users_in_batches(batch_size):
-        # Inner loop for users within the batch
         for user in batch:
             if user['age'] > 25:
                 print(user)
